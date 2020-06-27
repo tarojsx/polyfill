fa06204057561876094bc83e945b46785191cc99
colder <colder@vitarn.com>
2020-06-28 01:36:27 +0800

    test: add weapp e2e tests

---
+++ config/index.ts
@@ -0,0 +1,30 @@
+import path from 'path'
+import WebpackChain from 'webpack-chain'
+import { TaroProvidePlugin } from '../dist/plugins'
+import { IProjectConfig } from '@tarojs/taro/types/compile'
+
+const config: IProjectConfig = {
+    projectName: 'test',
+    date: '2020-2-22',
+    designWidth: 750,
+    deviceRatio: { 750: 1 },
+    sourceRoot: 'test/src',
+    outputRoot: 'test/dist',
+    framework: 'react',
+    alias: {
+        '@tarojsx/polyfill': path.resolve(__dirname, '..'),
+    },
+    mini: {
+        webpackChain(chain: WebpackChain) {
+            chain.optimization.sideEffects(false)
+            chain.plugin('taroProvidePlugin').use(TaroProvidePlugin, [['default', 'Intl']])
+        },
+    },
+}
+
+export default function (merge) {
+    if (process.env.NODE_ENV === 'development') {
+        return merge({}, config, { env: { NODE_ENV: '"development"' } })
+    }
+    return merge({}, config, { env: { NODE_ENV: '"production"' } })
+}
