6c8da95bffc411fccd65d7ac37f431e08ef5bc50
colder <colder@vitarn.com>
2020-06-28 14:46:38 +0800

    test: add storage tests

--- test/src/pages/index.tsx
+++ test/src/pages/index.tsx
@@ -4,16 +4,25 @@ import Taro from '@tarojs/taro'
 const polyfills = {
     fetch: fetch,
     'window.fetch': window.fetch,
+
     Intl: Intl,
     'window.Intl': window.Intl,
+
     navigator: navigator,
     'window.navigator': window.navigator,
+
     'performance.now': performance.now,
     'window.performance.now': window.performance.now,
+
     requestAnimationFrame: requestAnimationFrame,
     'window.requestAnimationFrame': window.requestAnimationFrame,
     cancelAnimationFrame: cancelAnimationFrame,
     'window.cancelAnimationFrame': window.cancelAnimationFrame,
+
+    localStorage: localStorage,
+    'window.localStorage': window.localStorage,
+    sessionStorage: sessionStorage,
+    'window.sessionStorage': window.sessionStorage,
 }
 
 export default () => {
--- test/weapp.test.ts
+++ test/weapp.test.ts
@@ -2,8 +2,40 @@
 
 import path from 'path'
 import automator from 'miniprogram-automator'
-import MiniProgram from 'miniprogram-automator/out/MiniProgram'
+import _MiniProgram from 'miniprogram-automator/out/MiniProgram'
 import Page from 'miniprogram-automator/out/Page'
+
+declare global {
+    interface App {
+        globalData: {
+            fetch: Window['fetch']
+            'window.fetch': Window['fetch']
+
+            Intl: typeof window.Intl
+            'window.Intl': typeof window.Intl
+
+            navigator: Navigator
+            'window.navigator': Navigator
+
+            'performance.now': Window['performance']['now']
+            'window.performance.now': Window['performance']['now']
+
+            requestAnimationFrame: Window['requestAnimationFrame']
+            'window.requestAnimationFrame': Window['requestAnimationFrame']
+            cancelAnimationFrame: Window['cancelAnimationFrame']
+            'window.cancelAnimationFrame': Window['cancelAnimationFrame']
+
+            localStorage: Storage
+            'window.localStorage': Storage
+            sessionStorage: Storage
+            'window.sessionStorage': Storage
+        }
+    }
+
+    class MiniProgram extends _MiniProgram {
+        evaluate<T = any>(appFunction: string | (() => T | Promise<T>)): Promise<T>
+    }
+}
 
 const cwd = path.resolve(__dirname, '..')
 
@@ -22,20 +54,19 @@ describe('weapp', () => {
     }, 30000)
 
     afterAll(async () => {
-        miniProgram && (await miniProgram.close())
+        // miniProgram && (await miniProgram.close())
     })
 
     describe('polyfill', () => {
-        it('have 6 polyfills and 6 window fields', async () => {
-            let res = await miniProgram.evaluate(() => Object.keys(getApp()['globalData']))
-            expect(res.length).toBe(6 * 2)
+        it('have 8 polyfills and 8 window fields', async () => {
+            await expect(miniProgram.evaluate(() => Object.keys(getApp().globalData))).resolves.toHaveLength(8 * 2)
         })
     })
 
     describe('fetch', () => {
         it('has fetch', async () => {
             let res = await miniProgram.evaluate(() => {
-                let { fetch, 'window.fetch': $fetch } = getApp()['globalData']
+                let { fetch, 'window.fetch': $fetch } = getApp().globalData
                 return {
                     same: fetch === $fetch,
                     type: typeof fetch,
@@ -48,7 +79,7 @@ describe('weapp', () => {
         it('fetch website', async () => {
             let res = await miniProgram.evaluate(() =>
                 (async () => {
-                    const fetch: Window['fetch'] = getApp()['globalData'].fetch
+                    const { fetch } = getApp().globalData
                     return await fetch('https://servicewechat.com')
                 })()
             )
@@ -59,7 +90,7 @@ describe('weapp', () => {
     describe('Intl', () => {
         it('has Intl', async () => {
             let res = await miniProgram.evaluate(() => {
-                let { Intl, 'window.Intl': $Intl } = getApp()['globalData']
+                let { Intl, 'window.Intl': $Intl } = getApp().globalData
                 return {
                     Intl,
                     $Intl,
@@ -73,7 +104,7 @@ describe('weapp', () => {
     describe('navigator', () => {
         it('has navigator', async () => {
             let res = await miniProgram.evaluate(() => {
-                let { navigator, 'window.navigator': $navigator } = getApp()['globalData']
+                let { navigator, 'window.navigator': $navigator } = getApp().globalData
                 return {
                     navigator,
                     $navigator,
@@ -84,18 +115,14 @@ describe('weapp', () => {
         })
 
         it('is Taro product', async () => {
-            let res = await miniProgram.evaluate(() => {
-                let navigator: Navigator = getApp()['globalData'].navigator
-                return navigator.product
-            })
-            expect(res).toBe('Taro')
+            await expect(miniProgram.evaluate(() => getApp().globalData.navigator.product)).resolves.toBe('Taro')
         })
     })
 
     describe('performance.now', () => {
         it('has performance.now', async () => {
             let res = await miniProgram.evaluate(() => {
-                let { 'performance.now': now, 'window.performance.now': $now } = getApp()['globalData']
+                let { 'performance.now': now, 'window.performance.now': $now } = getApp().globalData
                 return {
                     same: now === $now,
                     type: typeof now,
@@ -106,11 +133,9 @@ describe('weapp', () => {
         })
 
         it('return positive number', async () => {
-            let res = await miniProgram.evaluate(() => {
-                let now: Window['performance']['now'] = getApp()['globalData']['performance.now']
-                return now()
-            })
-            expect(res).toBeGreaterThan(0)
+            await expect(miniProgram.evaluate(() => getApp().globalData['performance.now']())).resolves.toBeGreaterThan(
+                0
+            )
         })
     })
 
@@ -122,7 +147,7 @@ describe('weapp', () => {
                     cancelAnimationFrame,
                     'window.requestAnimationFrame': $requestAnimationFrame,
                     'window.cancelAnimationFrame': $cancelAnimationFrame,
-                } = getApp()['globalData']
+                } = getApp().globalData
 
                 return {
                     same:
@@ -140,11 +165,9 @@ describe('weapp', () => {
         it('invoke callback', async () => {
             let res = await miniProgram.evaluate(() =>
                 (async () => {
-                    let data = getApp()['globalData']
-                    let requestAnimationFrame: Window['requestAnimationFrame'] = data.requestAnimationFrame
-                    let cancelAnimationFrame: Window['cancelAnimationFrame'] = data.cancelAnimationFrame
+                    let { requestAnimationFrame, cancelAnimationFrame } = getApp().globalData
 
-                    return await new Promise((resolve) => {
+                    return await new Promise(resolve => {
                         let timer = requestAnimationFrame(() => {
                             cancelAnimationFrame(timer)
                             resolve(true)
@@ -153,6 +176,151 @@ describe('weapp', () => {
                 })()
             )
             expect(res).toBe(true)
+        })
+    })
+
+    describe('localStorage', () => {
+        beforeEach(async () => await miniProgram.evaluate(() => wx.clearStorageSync()))
+
+        afterEach(async () => await miniProgram.evaluate(() => wx.clearStorageSync()))
+
+        it('has localStorage', async () => {
+            let res = await miniProgram.evaluate(() => {
+                let { localStorage, 'window.localStorage': $localStorage } = getApp().globalData
+
+                return {
+                    same: localStorage === $localStorage,
+                    type: typeof requestAnimationFrame,
+                }
+            })
+            expect(res.same).toBe(true)
+            expect(res.type).toBe('function')
+        })
+
+        it('store localStorage values', async () => {
+            /* { } */
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.length)).resolves.toBe(0)
+
+            await expect(
+                miniProgram.evaluate(() => getApp().globalData.localStorage.getItem('--non-exists-key--'))
+            ).resolves.toBeNull()
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.key(999))).resolves.toBeNull()
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.key(-1))).resolves.toBeNull()
+
+            /* { test: '' } */
+
+            await miniProgram.evaluate(() => getApp().globalData.localStorage.setItem('test', ''))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.getItem('test'))).resolves.toBe('\"\"')
+
+            /* { test: 'test' } */
+
+            await miniProgram.evaluate(() => getApp().globalData.localStorage.setItem('test', 'test'))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.getItem('test'))).resolves.toBe(
+                '"test"'
+            )
+            await expect(miniProgram.evaluate(() => wx.getStorageSync('test'))).resolves.toBe('"test"')
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.length)).resolves.toBe(1)
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.key(0))).resolves.toBe('test')
+
+            /* { test: { a: 1 } } */
+
+            await miniProgram.evaluate(() => wx.setStorageSync('test', { a: 1 }))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.getItem('test'))).resolves.toBe(
+                '{"a":1}'
+            )
+
+            /* { test: { a: 1 }, test2: 'test2' } */
+
+            await miniProgram.evaluate(() => getApp().globalData.localStorage.setItem('test2', 'test2'))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.length)).resolves.toBe(2)
+
+            /* { test: { a: 1 } } */
+
+            await miniProgram.evaluate(() => {
+                let { localStorage } = getApp().globalData
+                localStorage.removeItem('test-none')
+                localStorage.removeItem('test2')
+            })
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.length)).resolves.toBe(1)
+
+            /* { } */
+
+            await miniProgram.evaluate(() => getApp().globalData.localStorage.clear())
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.localStorage.length)).resolves.toBe(0)
+        })
+    })
+
+    describe('sessionStorage', () => {
+        it('has sessionStorage', async () => {
+            let res = await miniProgram.evaluate(() => {
+                let { sessionStorage, 'window.sessionStorage': $sessionStorage } = getApp().globalData
+
+                return {
+                    same: sessionStorage === $sessionStorage,
+                    type: typeof requestAnimationFrame,
+                }
+            })
+            expect(res.same).toBe(true)
+            expect(res.type).toBe('function')
+        })
+
+        it('store sessionStorage values', async () => {
+            /* { } */
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.length)).resolves.toBe(0)
+
+            await expect(
+                miniProgram.evaluate(() => getApp().globalData.sessionStorage.getItem('--non-exists-key--'))
+            ).resolves.toBeNull()
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.key(999))).resolves.toBeNull()
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.key(-1))).resolves.toBeNull()
+
+            /* { test: '' } */
+
+            await miniProgram.evaluate(() => getApp().globalData.sessionStorage.setItem('test', ''))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.getItem('test'))).resolves.toBe('\"\"')
+
+            /* { test: 'test' } */
+
+            await miniProgram.evaluate(() => getApp().globalData.sessionStorage.setItem('test', 'test'))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.length)).resolves.toBe(1)
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.key(0))).resolves.toBe('test')
+
+            /* { test: 'test', test2: 'test2' } */
+
+            await miniProgram.evaluate(() => getApp().globalData.sessionStorage.setItem('test2', 'test2'))
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.length)).resolves.toBe(2)
+
+            /* { test: 'test' } */
+
+            await miniProgram.evaluate(() => {
+                let { sessionStorage } = getApp().globalData
+                sessionStorage.removeItem('test-none')
+                sessionStorage.removeItem('test2')
+            })
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.length)).resolves.toBe(1)
+
+            /* { } */
+
+            await miniProgram.evaluate(() => getApp().globalData.sessionStorage.clear())
+
+            await expect(miniProgram.evaluate(() => getApp().globalData.sessionStorage.length)).resolves.toBe(0)
         })
     })
 })
